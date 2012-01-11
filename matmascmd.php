19eb90afdfd839a4649185c5a8d0967730f3666b
Martin Riesz <riesz.martin@gmail.com>
2012-01-11 03:17:11 +0100

    added license, GPLv3

--- matmascmd.php
+++ matmascmd.php
@@ -1,7 +1,19 @@
 <?php
-// Matmas Commander
-// by Matmas
-// All rights reserved
+// matmascmd (Matmas Commander)
+// Copyright (c) 2005 by Martin Riesz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 session_start();
 @set_time_limit(0);
